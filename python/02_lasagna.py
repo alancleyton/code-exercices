21dbe9e125a64b601e15b937a1e2c0a745f036ca
Alan Cleyton <alancleyton67@gmail.com>
2024-04-16 10:52:27 -0300

    rename file names

--- python/02_lasagna.py
+++ python/02_lasagna.py
@@ -12,7 +12,7 @@ def bake_time_remaining(elapsed_bake_time: int) -> int:
 
     :param elapsed_bake_time: int - baking time already elapsed.
     :return: int - remaining bake time (in minutes) derived from 'EXPECTED_BAKE_TIME'.
-  
+
     Function that takes the actual minutes the lasagna has been in the oven as
     an argument and returns how many minutes the lasagna still needs to bake
     based on the `EXPECTED_BAKE_TIME`.
